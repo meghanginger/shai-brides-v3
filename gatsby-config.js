2eea304afbb2230681c119183f5c6ab964d0be2e
Meghan Avery <Meghan@Meghans-Mega.local>
2021-01-03 13:51:58 +0000

    init tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,19 @@
 module.exports = {
   siteMetadata: {
-    title: "shai-brides-v3",
-  },
-  plugins: ["gatsby-plugin-sass"],
-};
+      title: `B29 TOUR`,
+      description: `February 2021`,
+      author: `@b29music`,
+      image: `/images/bg-min.jpg`
+    },
+  plugins: [
+      {
+          resolve: `gatsby-plugin-sass`,
+          options: { 
+              postCssPlugins: [require("tailwindcss")()],
+          },
+      },
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      { resolve: `gatsby-source-filesystem`, options: { path: `./src/images/` } },
+  ],
+}
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ['Manrope', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        gray: colors.coolGray,
+        blue: colors.lightBlue,
+        red: colors.rose,
+        pink: colors.fuchsia,
+        black: colors.black,
+        beige: "#d3ccbc"
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    }
+  },
+  variants: {
+    extend: {
+      transitionDuration: ['hover', 'focus'],
+    },
+  },
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("postcss-nested"),
+    require("autoprefixer"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/custom-forms"),
+  ],
+}
